ad931f87506dbbe43df35dcbbc4c9462617f5213
xsauce@gmail.com <gxsauce0125>
2016-08-12 23:48:17 +0800

    9

--- 9_PalindromeNumber.py
+++ 9_PalindromeNumber.py
@@ -4,13 +4,45 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if -9 <= x <= 9:
-            return x
-        x = abs(x)
-        while x > 0:
+        if 0 <= x <= 9:
+            return True
+        if x < 0:
+            return False
+        r = 0
+        max_int32 = 2 ** 31 - 1
+        temp_x = x
+        while temp_x > 0:
+            if r > max_int32 / 10:
+                return False
+            r = r * 10 + temp_x % 10
+            temp_x = temp_x // 10
+            if r == temp_x:
+                return True
+
+        return r == x
+
+class Solution1(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+
+        r = 0
+        while x > r:
+            r = r * 10 + x % 10
             x = x // 10
+        return r == x or r / 10 == x
 
 
 
-s = Solution()
-print s.isPalindrome(0), 0
+
+
+s = Solution1()
+print s.isPalindrome(0), True
+print s.isPalindrome(-1), False
+print s.isPalindrome(12344321), True
+print s.isPalindrome(12321), True
+print s.isPalindrome(1000000000003), False
